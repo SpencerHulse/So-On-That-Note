7f7c730b7bcfc1dcae47e13c3901664acd672727
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-15 00:07:07 -0500

    Added the api/notes/:id get route and logic.

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { createNewNote } = require("../../lib/notes");
+const { createNewNote, findNoteById } = require("../../lib/notes");
 const { notes } = require("../../db/db.json");
 
 router.get("/notes", (req, res) => {
@@ -8,9 +8,20 @@ router.get("/notes", (req, res) => {
   res.status(200).json(results);
 });
 
+router.get("/notes/:id", (req, res) => {
+  const result = findNoteById(req.params.id, notes);
+  if (result) {
+    res.status(200).json(result);
+  } else {
+    res.status(404).sendStatus(404);
+  }
+});
+
 router.post("/notes", (req, res) => {
   const note = createNewNote(req.body, notes);
   res.json(note);
 });
+
+router.delete("/notes", (req, res) => {});
 
 module.exports = router;
