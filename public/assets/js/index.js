a1c7c6c324ab8e9fc2eed3c5c8dbbf00a1512d9e
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-15 02:36:55 -0500

    Updated name in HTML.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -98,7 +98,6 @@ const handleNoteDelete = (e) => {
 // Sets the activeNote and displays it
 const handleNoteView = (e) => {
   e.preventDefault();
-
   // prevents an error when there are no notes and
   // you click on the No saved Notes placeholder note
   if (e.target.textContent === "No saved Notes") {
