9cfbb3737cdd10f4f232441666c15136326684d1
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-17 02:27:37 -0500

    Refactored lib controllers and routes.

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,30 +1,16 @@
-const router = require("express").Router();
-const { createNewNote, findNoteById, deleteNote } = require("../../lib/notes");
-const { notes } = require("../../db/db.json");
+const express = require("express");
+const router = express.Router();
 
-router.get("/notes", (req, res) => {
-  const results = notes;
+// Controller variables
+const {
+  getAllNotes,
+  createNewNote,
+  findNoteById,
+  deleteNote,
+} = require("../../lib/notes");
 
-  res.status(200).json(results);
-});
-
-router.get("/notes/:id", (req, res) => {
-  const result = findNoteById(req.params.id, notes);
-  if (result) {
-    res.status(200).json(result);
-  } else {
-    res.status(404).sendStatus(404);
-  }
-});
-
-router.post("/notes", (req, res) => {
-  const note = createNewNote(req.body, notes);
-  res.json(note);
-});
-
-router.delete("/notes/:id", (req, res) => {
-  const newNotes = deleteNote(req.params.id, notes);
-  res.json(newNotes);
-});
+// Routes
+router.route("/notes").get(getAllNotes).post(createNewNote);
+router.route("/notes/:id").get(findNoteById).delete(deleteNote);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require("express");
+const app = express();
+
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
+// Routes
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.listen(PORT, (req, res) => {
-  console.log("The server is listening on port 3001");
+// Port
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`The server is listening on port ${PORT}`);
 });
