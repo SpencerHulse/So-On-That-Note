e4a785e720df46a6a2f673fc45524775be6ee367
Spencer Hulse <hulse.spencer@gmail.com>
2022-02-17 04:16:13 -0500

    Change for heroku.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("./public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-// Port
+// Port - ENV URL
 const PORT = process.env.PORT || 3001;
 const start = async () => {
   try {
